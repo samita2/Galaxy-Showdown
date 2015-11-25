63d56f636534494394bdc02a130453435ae0a794
Flareninja <flareninja98@gmail.com>
2015-11-25 15:37:06 +0100

    Update poll.js

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -1,135 +1,297 @@
-var Poll = {
-	reset: function (roomId) {
-		Poll[roomId] = {
-			question: undefined,
-			optionList: [],
-			options: {},
-			display: '',
-			topOption: ''
-		};
-	},
+/*
+* Poll chat plugin
+* By bumbadadabum and Zarel.
+*/
 
-	splint: function (target) {
-		var parts = target.split(',');
-		var len = parts.length;
-		while (len--) {
-			parts[len] = parts[len].trim();
+'use strict';
+
+const permission = 'announce';
+
+let Poll = (function () {
+	function Poll(room, question, options) {
+		if (room.pollNumber) {
+			room.pollNumber++;
+		} else {
+			room.pollNumber = 1;
 		}
-		return parts;
+		this.room = room;
+		this.question = question;
+		this.voters = {};
+		this.voterIps = {};
+		this.totalVotes = 0;
+		this.timeout = null;
+		this.timeoutMins = 0;
+
+		this.options = new Map();
+		for (let i = 0; i < options.length; i++) {
+			this.options.set(i + 1, {name: options[i], votes: 0});
+		}
 	}
-};
 
-for (var id in Rooms.rooms) {
-	if (Rooms.rooms[id].type === 'chat' && !Poll[id]) {
-		Poll[id] = {};
-		Poll.reset(id);
-	}
-}
+	Poll.prototype.vote = function (user, option) {
+		let ip = user.latestIp;
+		let userid = user.userid;
 
-exports.commands = {
-	poll: function (target, room, user) {
-		if (!this.can('broadcast', null, room)) return false;
-		if (!Poll[room.id]) Poll.reset(room.id);
-		if (Poll[room.id].question) return this.sendReply("There is currently a poll going on already.");
-		if (!this.canTalk()) return;
-
-		var options = Poll.splint(target);
-		if (options.length < 3) return this.parse('/help poll');
-
-		var question = options.shift();
-
-		options = options.join(',').split(',');
-
-		Poll[room.id].question = question;
-		Poll[room.id].optionList = options;
-
-		var pollOptions = '';
-		var start = 0;
-		while (start < Poll[room.id].optionList.length) {
-			pollOptions += '<button name="send" value="/vote ' + Tools.escapeHTML(Poll[room.id].optionList[start]) + '">' + Tools.escapeHTML(Poll[room.id].optionList[start]) + '</button>&nbsp;';
-			start++;
-		}
-		Poll[room.id].display = '<h2>' + Tools.escapeHTML(Poll[room.id].question) + '&nbsp;&nbsp;<font size="1" color="#AAAAAA">/vote OPTION</font><br><font size="1" color="#AAAAAA">Poll started by <em>' + user.name + '</em></font><br><hr>&nbsp;&nbsp;&nbsp;&nbsp;' + pollOptions;
-		room.add('|raw|<div class="infobox">' + Poll[room.id].display + '</div>');
-	},
-
-	pollhelp: ["/poll [question], [option 1], [option 2]... - Create a poll where users can vote on an option."],
-	endpoll: function (target, room, user) {
-		if (!this.can('broadcast', null, room)) return false;
-		if (!Poll[room.id]) Poll.reset(room.id);
-		if (!Poll[room.id].question) return this.sendReply("There is no poll to end in this room.");
-
-		var votes = Object.keys(Poll[room.id].options).length;
-
-		if (votes === 0) {
-			Poll.reset(room.id);
-			return room.add('|raw|<h3>The poll was canceled because of lack of voters.</h3>');
+		if (userid in this.voters || ip in this.voterIps) {
+			return user.sendTo(this.room, "You have already voted for this poll.");
 		}
 
-		var options = {};
+		this.voters[userid] = option;
+		this.voterIps[ip] = option;
+		this.options.get(option).votes++;
+		this.totalVotes++;
 
-		for (var l in Poll[room.id].optionList) {
-			options[Poll[room.id].optionList[l]] = 0;
+		this.update();
+	};
+
+	Poll.prototype.blankvote = function (user, option) {
+		let ip = user.latestIp;
+		let userid = user.userid;
+
+		if (userid in this.voters || ip in this.voterIps) {
+			return user.sendTo(this.room, "You're already looking at the results.");
+		} else {
+			this.voters[userid] = 0;
+			this.voterIps[ip] = 0;
 		}
 
-		for (var o in Poll[room.id].options) {
-			options[Poll[room.id].options[o]]++;
-		}
+		this.update();
+	};
 
-		var data = [];
-		for (var i in options) {
-			data.push([i, options[i]]);
-		}
-		data.sort(function (a, b) {
-			return a[1] - b[1];
+	Poll.prototype.generateVotes = function () {
+		let output = '<div class="infobox"><p style="margin: 2px 0 5px 0"><span style="border:1px solid #6A6;color:#484;border-radius:4px;padding:0 3px"><i class="fa fa-bar-chart"></i> Poll</span> <strong style="font-size:11pt">' + Tools.escapeHTML(this.question) + '</strong></p>';
+		this.options.forEach(function (option, number) {
+			output += '<div style="margin-top: 3px"><button value="/poll vote ' + number + '" name="send" title="Vote for ' + number + '. ' + Tools.escapeHTML(option.name) + '">' + number + '. <strong>' + Tools.escapeHTML(option.name) + '</strong></button></div>';
 		});
+		output += '</div>';
 
-		var results = '';
-		var len = data.length;
-		var topOption = data[len - 1][0];
-		while (len--) {
-			if (data[len][1] > 0) {
-				results += '&bull; ' + data[len][0] + ' - ' + Math.floor(data[len][1] / votes * 100) + '% (' + data[len][1] + ')<br>';
+		return output;
+	};
+
+	Poll.prototype.generateResults = function (ended, option) {
+		let icon = '<span style="border:1px solid #' + (ended ? '777;color:#555' : '6A6;color:#484') + ';border-radius:4px;padding:0 3px"><i class="fa fa-bar-chart"></i> ' + (ended ? "Poll ended" : "Poll") + '</span>';
+		let output = '<div class="infobox"><p style="margin: 2px 0 5px 0">' + icon + ' <strong style="font-size:11pt">' + Tools.escapeHTML(this.question) + '</strong></p>';
+		let iter = this.options.entries();
+
+		let i = iter.next();
+		let c = 0;
+		let colors = ['#79A', '#8A8', '#88B'];
+		while (!i.done) {
+			let percentage = Math.round((i.value[1].votes * 100) / (this.totalVotes || 1));
+			output += '<div style="margin-top: 3px">' + i.value[0] + '. <strong>' + (i.value[0] === option ? '<em>' : '') + Tools.escapeHTML(i.value[1].name) + (i.value[0] === option ? '</em>' : '') + '</strong> <small>(' + i.value[1].votes + ' vote' + (i.value[1].votes === 1 ? '' : 's') + ')</small><br /><span style="font-size:7pt;background:' + colors[c % 3] + ';padding-right:' + (percentage * 3) + 'px"></span><small>&nbsp;' + percentage + '%</small></div>';
+			i = iter.next();
+			c++;
+		}
+		output += '</div>';
+
+		return output;
+	};
+
+	Poll.prototype.update = function () {
+		let results = [];
+
+		for (let i = 0; i <= this.options.size; i++) {
+			results.push(this.generateResults(false, i));
+		}
+
+		// Update the poll results for everyone that has voted
+		for (let i in this.room.users) {
+			let user = this.room.users[i];
+			if (user.userid in this.voters) {
+				user.sendTo(this.room, '|uhtmlchange|poll' + this.room.pollNumber + '|' + results[this.voters[user.userid]]);
+			} else if (user.latestIp in this.voterIps) {
+				user.sendTo(this.room, '|uhtmlchange|poll' + this.room.pollNumber + '|' + results[this.voterIps[user.latestIp]]);
 			}
 		}
-		room.add('|raw|<div class="infobox"><h2>Results to "' + Poll[room.id].question + '"</h2><font size="1" color="#AAAAAA"><strong>Poll ended by <em>' + user.name + '</em></font><br><hr>' + results + '</strong></div>');
-		Poll.reset(room.id);
-		Poll[room.id].topOption = topOption;
-	},
+	};
 
-	pr: 'pollremind',
-	pollremind: function (target, room, user) {
-		if (!Poll[room.id]) Poll.reset(room.id);
-		if (!Poll[room.id].question) return this.sendReply("There is no poll currently going on in this room.");
+	Poll.prototype.display = function (user, broadcast) {
+		let votes = this.generateVotes();
+
+		let results = [];
+
+		for (let i = 0; i <= this.options.size; i++) {
+			results.push(this.generateResults(false, i));
+		}
+
+		let target = {};
+
+		if (broadcast) {
+			target = this.room.users;
+		} else {
+			target[0] = user;
+		}
+
+		for (let i in target) {
+			let thisUser = target[i];
+			if (thisUser.userid in this.voters) {
+				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + results[this.voters[thisUser.userid]]);
+			} else if (thisUser.latestIp in this.voterIps) {
+				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + results[this.voterIps[thisUser.latestIp]]);
+			} else {
+				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + votes);
+			}
+		}
+	};
+
+	Poll.prototype.end = function () {
+		let results = this.generateResults(true);
+
+		this.room.send('|uhtmlchange|poll' + this.room.pollNumber + '|<div class="infobox">(The poll has ended &ndash; scroll down to see the results)</div>');
+		this.room.send('|html|' + results);
+	};
+
+	return Poll;
+})();
+
+exports.commands = {
+	poll: {
+		create: 'new',
+		new: function (target, room, user, connection, cmd, message) {
+			if (!target) return this.parse('/help poll new');
+			if (target.length > 1024) return this.errorReply("Poll too long.");
+			let params = target.split(target.includes('|') ? '|' : ',').map(function (param) { return param.trim(); });
+
+			if (!this.can(permission, null, room)) return false;
+			if (!this.canTalk()) return this.errorReply("You cannot do this while unable to talk.");
+			if (room.poll) return this.errorReply("There is already a poll in progress in this room.");
+
+			if (params.length < 3) return this.errorReply("Not enough arguments for /poll new.");
+			let options = [];
+
+			for (let i = 1; i < params.length; i++) {
+				options.push(params[i]);
+			}
+
+			if (options.length > 10) {
+				return this.errorReply("Too many options for poll (maximum is 10).");
+			}
+
+			room.poll = new Poll(room, params[0], options);
+			room.poll.display(user, true);
+
+			this.logEntry("" + user.name + " used " + message);
+			return this.privateModCommand("(A poll was started by " + user.name + ".)");
+		},
+		newhelp: ["/poll create [question], [option1], [option2], [...] - Creates a poll. Requires: % @ # & ~"],
+
+		vote: function (target, room, user) {
+			if (!room.poll) return this.errorReply("There is no poll running in this room.");
+			if (!target) return this.parse('/help poll vote');
+
+			if (target === 'blank') {
+				room.poll.blankvote(user);
+				return;
+			}
+
+			let parsed = parseInt(target, 10);
+			if (isNaN(parsed)) return this.errorReply("To vote, specify the number of the option.");
+
+			if (!room.poll.options.has(parsed)) return this.sendReply("Option not in poll.");
+
+			room.poll.vote(user, parsed);
+		},
+		votehelp: ["/poll vote [number] - Votes for option [number]."],
+
+		timer: function (target, room, user) {
+			if (!room.poll) return this.errorReply("There is no poll running in this room.");
+
+			if (target) {
+				if (!this.can(permission, null, room)) return false;
+				if (target === 'clear') {
+					if (!room.poll.timeout) return this.errorReply("There is no timer to clear.");
+					clearTimeout(room.poll.timeout);
+					room.poll.timeout = null;
+					room.poll.timeoutMins = 0;
+					return this.add("The timeout for the poll was cleared.");
+				}
+				let timeout = parseFloat(target);
+				if (isNaN(timeout) || timeout <= 0 || timeout > 0x7FFFFFFF) return this.errorReply("Invalid time given.");
+				if (room.poll.timeout) clearTimeout(room.poll.timeout);
+				room.poll.timeoutMins = timeout;
+				room.poll.timeout = setTimeout((function () {
+					room.poll.end();
+					delete room.poll;
+				}), (timeout * 60000));
+				room.add("The timeout for the poll was set to " + timeout + " minutes.");
+				return this.privateModCommand("(The poll timeout was set to " + timeout + " minutes by " + user.name + ".)");
+			} else {
+				if (!this.canBroadcast()) return;
+				if (room.poll.timeout) {
+					return this.sendReply("The timeout for the poll is " + room.poll.timeoutMins + " minutes.");
+				} else {
+					return this.sendReply("There's no timer for this poll.");
+				}
+			}
+		},
+		timerhelp: ["/poll timer [minutes] - Sets the poll to automatically end after [minutes] minutes. Requires: % @ # & ~", "/poll timer clear - Clears the poll's timer. Requires: % @ # & ~"],
+
+		results: function (target, room, user) {
+			if (!room.poll) return this.errorReply("There is no poll running in this room.");
+
+			return room.poll.blankvote(user);
+		},
+		resultshelp: ["/poll results - Shows the results of the poll without voting. NOTE: you can't go back and vote after using this."],
+
+		close: 'end',
+		stop: 'end',
+		end: function (target, room, user) {
+			if (!this.can(permission, null, room)) return false;
+			if (!this.canTalk()) return this.errorReply("You cannot do this while unable to talk.");
+			if (!room.poll) return this.errorReply("There is no poll running in this room.");
+			if (room.poll.timeout) clearTimeout(room.poll.timeout);
+
+			room.poll.end();
+			delete room.poll;
+			return this.privateModCommand("(The poll was ended by " + user.name + ".)");
+		},
+		endhelp: ["/poll end - Ends a poll and displays the results. Requires: % @ # & ~"],
+
+		show: 'display',
+		display: function (target, room, user) {
+			if (!room.poll) return this.errorReply("There is no poll running in this room.");
+			if (!this.canBroadcast()) return;
+			room.update();
+
+			room.poll.display(user, this.broadcasting);
+		},
+		displayhelp: ["/poll display - Displays the poll"],
+
+		'': function (target, room, user) {
+			this.parse('/help poll');
+		}
+	},
+	pollhelp: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox(Poll[room.id].display);
+		return this.sendReplyBox(
+			"/poll allows rooms to run their own polls. These polls are limited to one poll at a time per room.<br />" +
+			"The poll status is displayed to the users and updated in real time.<br />" +
+			"Accepts the following commands:<br />" +
+			"/poll create [question], [option1], [option2], [...] - Creates a poll. Requires: + % @ # & ~<br />" +
+			"/poll vote [number] - Votes for option [number] (or click the button).<br />" +
+			"/poll timer [minutes] - Sets the poll to automatically end after [minutes]. Requires: + % @ # & ~<br />" +
+			"/poll display - Displays the poll<br />" +
+			"/poll end - Ends a poll and displays the results. Requires: + % @ # & ~"
+		);
 	},
-
-	formatpoll: 'tierpoll',
-	tpoll: 'tierpoll',
-	tierspoll: 'tierpoll',
-	tierpoll: function (target, room, user) {
-		if (!this.can('broadcast', null, room)) return false;
-		this.parse("/poll Tournament tier?," + "Random Battle, OU, Ubers, UU, RU, NU, LC, Anything Goes, BW2 OU, DPP OU, ADV OU, GSC OU, RBY OU, Battle Spot Singles, Random Doubles Battle, Doubles OU, VGC 2015, Random Triples Battle, Smogon Triples, CAP, Battle Factory, Challenge Cup 1v1, Balanced Hackmons, 1v1, Monotype, Tier Shift, PU, Inverse Battle, Monotype Random Battle, LC UU, FU, Middle Cup, Metronome, Random Haxmons, Random Inverse, Random Cap, Random Lc, Random Monogen, Random Ubers, RBY Random, GSC Random, VGC 2014, VGC 2013, VGC 2010, Stabmons, AAA, Alphabet Cup, Mega Tier");
-	},
-
-	vote: function (target, room, user) {
-		if (!Poll[room.id]) Poll.reset(room.id);
-		if (!Poll[room.id].question) return this.sendReply("There is no poll currently going on in this room.");
-		if (!target) return this.parse('/help vote');
-		if (Poll[room.id].optionList.indexOf(target) === -1) return this.sendReply("'" + target + "' is not an option for the current poll.");
-
-		var ips = JSON.stringify(user.ips);
-		Poll[room.id].options[ips] = target;
-
-		return this.sendReply("You are now voting for " + target + ".");
-	},
-	votehelp: ["/vote [option] - Vote for an option in the poll."],
-
-	votes: function (target, room, user) {
+	votes: function(target, room, user) {
+		if (!room.poll) return this.errorReply("There is no poll running in this room.");
 		if (!this.canBroadcast()) return;
-		if (!Poll[room.id]) Poll.reset(room.id);
-		if (!Poll[room.id].question) return this.sendReply("There is no poll currently going on in this room.");
-		this.sendReply("NUMBER OF VOTES: " + Object.keys(Poll[room.id].options).length);
+		var votes = room.poll.totalVotes;
+		var lbl = (votes > 1 ? ' VOTES' : ' VOTE');
+		return this.sendReplyBox("TOTAL VOTES: " + votes + lbl);
+	},
+	ep: 'endpoll',
+	endpoll: function(target, room, user) {
+		this.parse('/poll end');
+	},
+	tierpoll: 'tpoll',
+	tpoll: function(target, room, user) {
+		var tiers = ['OU', 'VGC', 'Random Meta', 'Ubers', 'UU', 'RU', 'NU', 'PU', 'LC', 'Old Gen', 'Other Metagame'];
+		this.parse('/poll new Next tournament tier?, ' + tiers.sort());
+	},
+	vote: function(target, room, user) {
+		if (!target) return this.errorReply("Usage: /vote [poll option number] - votes for the [option] in the current poll.");
+		this.parse('/poll vote ' + target);
 	}
 };
