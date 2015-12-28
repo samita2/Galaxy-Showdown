37fd6f9054fdaf679dcbadee115fb6c854703f12
Flareninja <flareninja98@gmail.com>
2015-12-28 20:00:17 +0100

    Update galaxy.js

--- chat-plugins/galaxy.js
+++ chat-plugins/galaxy.js
@@ -1092,6 +1092,48 @@ exports.commands = {
 			self.sendReply('|html|<img src = "' + url + sprite + alt + '.png">');
 		});
 	},
+	xyspr: 'xysprite',
+        xysprite: function (target, room, user, connection, cmd) {
+		if (!this.canBroadcast()) return;
+		if (!toId(target)) return this.sendReply('/xysprite [Pokémon] - Allows you to view the sprite of a Pokémon');
+		target = target.toLowerCase().split(',');
+		var alt = '';
+		var type = toId(target[1]);
+		var sprite = target[0].trim();
+		var url;
+		if (type === 'shiny') url = 'http://play.pokemonshowdown.com/sprites/xydex-shiny/';
+		else url = 'http://play.pokemonshowdown.com/sprites/xydex/';
+
+		if (Number(sprite[sprite.length - 1]) && !toId(sprite[sprite.length - 2])) {
+			alt = '-' + sprite[sprite.length - 1];
+			sprite = sprite.substr(0, sprite.length - 1);
+			url = 'http://www.pkparaiso.com/imagenes/xy/sprites/animados/';
+		}
+		var main = target[0].split(',');
+		if (Tools.data.Pokedex[toId(sprite)]) {
+			sprite = Tools.data.Pokedex[toId(sprite)].species.toLowerCase();
+		} else {
+			var correction = Tools.dataSearch(toId(sprite));
+			if (correction && correction.length) {
+				for (var i = 0; i < correction.length; i++) {
+					if (correction[i].id !== toId(sprite) && !Tools.data.Aliases[toId(correction[i].id)] && !i) {
+						if (!Tools.data.Pokedex[toId(correction[i])]) continue;
+						if (!Tools.data.Aliases[toId(sprite)]) this.sendReply("There isn't any Pokémon called '" + sprite + "'... Did you mean '" + correction[0].name + "'?\n");
+						sprite = Tools.data.Pokedex[correction[0].id].species.toLowerCase();
+					}
+				}
+			} else {
+				return this.sendReply("There isn\'t any Pokémon called '" + sprite + "'...");
+			}
+		}
+		var self = this;
+		require('request').get(url + sprite + alt + '.png').on('error', function () {
+			self.sendReply('The sprite for ' + sprite + alt + ' is unavailable.');
+		}).on('response', function (response) {
+			if (response.statusCode == 404) return self.sendReply('The sprite for ' + sprite + alt + ' is currently unavailable.');
+			self.sendReply('|html|<img src = "' + url + sprite + alt + '.png">');
+		});
+	},
         
     cries: function (target) {
 		if (!this.canBroadcast()) return; 
