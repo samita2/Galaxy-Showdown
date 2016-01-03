7126d8092eaaf1f6827f14b5d79762a0e724f262
Flareninja <flareninja98@gmail.com>
2016-01-03 14:52:19 +0100

    Update royale.js

--- chat-plugins/royale.js
+++ chat-plugins/royale.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let fs = require('fs');
+var fs = require('fs');
 
-let royale = JSON.parse(fs.readFileSync('config/royale.txt'));
+var royale = JSON.parse(fs.readFileSync('config/royale.txt'));
 
 exports.commands = {
 	setroyale: function (target, room, user, connection, cmd) {
 		if (!this.can('potd')) return;
-		let targets = target.split(",");
-		let seat = isNaN(targets[0]);
-		let len = royale.length;
+		var targets = target.split(",");
+		var seat = isNaN(targets[0]);
+		var len = royale.length;
 	if (!seat && targets[0] > 0 && targets[0] <= 7 && len < 10) {
 		royale[royale.length] = '<tr> <td>' + targets[0] + '</td> <td>' + targets[1] + '</td> <td>' + targets[2] + '</td> </tr>';
 	fs.writeFile('config/royale.txt', JSON.stringify(royale));
@@ -22,12 +22,12 @@ exports.commands = {
 	removeroyale: function (target, room, user, connection, cmd) {
 		if (!this.can('potd')) return;
 		
-		let len = royale.length;
+		var len = royale.length;
 			
 				if (len <= 0) {
 					this.sendReply("Royale List is currently empty.....")
 				} else { 
-		let royaleSpliced = royale.splice((target - 1),1);
+		var royaleSpliced = royale.splice((target - 1),1);
 		this.sendReply('|raw|<div class="infobox"><center><u><b>Royale Family</b></u></center><br><table border="1" cellspacing="0" cellpadding="5" width="100%"><tbody><tr><th>Seat</th><th>Username</th><th>Title</th></tr>' + royaleSpliced + '</table> </div>') 
 		fs.writeFile('config/royale.txt', JSON.stringify(royale));
 		this.sendReply("Successfully removed from list of royale.");
@@ -37,14 +37,14 @@ exports.commands = {
 	replaceroyale: function (target, room, user, connection, cmd) {
 		if (!this.can('potd')) return;
 		
-		let targets = target.split(",");
+		var targets = target.split(",");
 		
-		let len = royale.length;
+		var len = royale.length;
 			
 				if (len <= 0) {
 					this.sendReply("Royale List is currently empty.....")
 				} else { 
-		let royaleSpliced = royale.splice((targets[0] - 1),1);
+		var royaleSpliced = royale.splice((targets[0] - 1),1);
 		royale[royale.length] = '<tr> <td>' + targets[0] + '</td> <td>' + targets[1] + '</td> <td>' + targets[2] + '</td> </tr>';
 		fs.writeFile('config/royale.txt', JSON.stringify(royale));
 		this.sendReply("Successfully replaced Seat No. " + targets[0] + " from list of royale.");
@@ -55,15 +55,15 @@ exports.commands = {
 	royale: function (target, room, user, connection, cmd) {
 		if (!this.canBroadcast());
 
-			let len = royale.length;
+			var len = royale.length;
 			
 				if (len <= 0) {
 					this.sendReply("Royale List is currently empty.....")
 				} else { 
 			
 
-			let sortroyale = royale.sort();
-			let joinedroyale = sortroyale.join("");
+			var sortroyale = royale.sort();
+			var joinedroyale = sortroyale.join("");
 			this.sendReply('|raw|<div class="infobox"><center><u><b>Royale Family</b></u></center><br><table border="1" cellspacing="0" cellpadding="5" width="100%"><tbody><tr><th>Seat</th><th>Username</th><th>Title</th></tr>' + joinedroyale + '</table> </div>')
 				}
 				},
